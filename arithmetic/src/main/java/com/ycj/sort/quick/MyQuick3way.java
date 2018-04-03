4ab846e3ec484229e6025277ebb40d778d9cda52
chongjie.yuan <chongjie.yuan@mtime.com>
2018-04-03 11:16:21 +0800

    重写快速排序

--- arithmetic/src/main/java/com/ycj/sort/quick/MyQuick3way.java
+++ arithmetic/src/main/java/com/ycj/sort/quick/MyQuick3way.java
@@ -7,15 +7,15 @@ import org.springframework.util.Assert;
 
 import java.util.Arrays;
 
-public class MyQuick implements Sort<Integer> {
+public class MyQuick3way implements Sort<Integer> {
     @Override
     public void sort(Integer[] a) {
 
-        quickSort(a, 0, a.length-1);
+        quickSort(a, 0, a.length - 1);
     }
 
     public void quickSort(Integer[] a, int l, int h) {
-        if (l>=h) {
+        if (l >= h) {
             return;
         }
 
@@ -23,46 +23,37 @@ public class MyQuick implements Sort<Integer> {
         quickSort(a, l, resultIndex);
         quickSort(a, resultIndex + 1, h);
 
-
     }
 
     public int partition(Integer[] a, int l, int h) {
 
-
         int p = a[l];
-        int low = l;
-        int high = h+1;
-        while(true){
 
+        int low = l + 1, high = h;
+        while (low <= high) {
+            if (less(a[low], p)) {
+                ++low;
+            } else if (less(p, a[low])) {
+                exch(a, low, high);
+                --high;
 
-            while(a[++low]<p){
-                if(low==h){
-                    break;
-                }
+            } else {
+                ++low;
             }
-            while(a[--high]>p){
-                if(high==l){
-                    break;
-                }
-            }
-            if(low>=high){
-                break;
-            }
-            exch(a,low,high);
         }
-        exch(a,l,high);
+        exch(a, l, high);
         return high;
 
     }
 
     public static void main(String[] args) {
 
-        Integer[] source = ArrayDataUtil.getUniqueRandomIntArray(10000, 100000);
+        Integer[] source = ArrayDataUtil.getUniqueRandomIntArray(1000, 10000);
         System.out.println(Arrays.toString(source));
         Stopwatch stopwatch = Stopwatch.createStarted();
-        MyQuick quick = new MyQuick();
+        MyQuick3way quick = new MyQuick3way();
         quick.sort(source);
-        Assert.isTrue(quick.isSorted(source), "数组不是有序");
+        Assert.isTrue(quick.isSorted(source),"数组不是有序");
         System.out.println(Arrays.toString(source));
         System.out.println(stopwatch.elapsed()
                                     .toMillis());
